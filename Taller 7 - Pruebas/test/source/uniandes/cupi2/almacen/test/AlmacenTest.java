7a72f3082ff072dfd33d758c0ad1402a054f6f65
Camilo Morillo <c.morilloq@uniandes.edu.co>
2021-11-30 21:27:01 -0500

    Creacion de metodos clases tests

--- Taller 7 - Pruebas/test/source/uniandes/cupi2/almacen/test/AlmacenTest.java
+++ Taller 7 - Pruebas/test/source/uniandes/cupi2/almacen/test/AlmacenTest.java
@@ -2,6 +2,39 @@ package uniandes.cupi2.almacen.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import uniandes.cupi2.almacen.mundo.Almacen;
+
 public class AlmacenTest {
+	
+	private Almacen almacen;
+	
+	public void setupescenario1() {
+		
+	}
+	
+	public void testAgregarNodo() {
+		
+	}
+	
+	public void testEliminarNodo() {
+		
+	}
+	
+	public void testVenderProducto(){
+		
+	}
+	
+	public void testBuscarNodo() {
+		
+	}
+	
+	public void testAgregarProducto() {
+		
+	}
+	
+	public void testEliminarProducto() {
+		
+	}
+	
 
 }
--- Taller 7 - Pruebas/test/source/uniandes/cupi2/almacen/test/CategoriaTest.java
+++ Taller 7 - Pruebas/test/source/uniandes/cupi2/almacen/test/CategoriaTest.java
@@ -2,7 +2,62 @@ package uniandes.cupi2.almacen.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import uniandes.cupi2.almacen.mundo.Categoria;
+
 
 public class CategoriaTest {
 
+	private Categoria categoria;
+	
+	public void setupescenario1() {
+		
+	}
+	
+	public void setupescenario2() {
+		
+	}
+	
+	public void setupescenario3() {
+		
+	}
+	
+	public void testAgregarNodo() {
+		
+	}
+	
+	public void testBuscarNodo() {
+		
+	}
+	
+	public void testDarValorVentas() {
+		
+	}
+	
+	public void testEliminarNodo() {
+		
+	}
+	
+	public void testBuscarProducto() {
+		
+	}
+	
+	public void testBuscarPadre() {
+		
+	}
+	
+	public void testDarProductos() {
+		
+	}
+	
+	public void testDarMarcas() {
+		
+	}
+	
+	public void testDarPreorden() {
+		
+	}
+	
+	public void testDarPosorden() {
+		
+	}
 }
